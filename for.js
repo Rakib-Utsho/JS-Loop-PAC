4afa1fb9ae6e68a1033367f10719e8a9accac69a
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-03 00:58:29 +0600

    first commit

--- for.js
+++ for.js
@@ -5,6 +5,16 @@
 // }
 
 // for loop
-for(var rostGiven = 0;rostGiven<7;rostGiven++){
-    console.log("Rost Den,Please!!")
+// for(var rostGiven = 0;rostGiven<7;rostGiven++){
+//     console.log("Rost Den,Please!!")
+// }
+
+// simple version for loop
+/* console. log("simple for loop");
+for(var i=0;i<7;i++){
+    console.log(i);
+} */
+
+for(var i=0;i<=10;i++){
+    console.log(i)
 }
