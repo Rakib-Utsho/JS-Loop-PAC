67404b2c6edf972825a265836370733cda97d20c
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-03 01:52:52 +0600

    module 19 done

---
+++ reverse.js
@@ -0,0 +1,32 @@
+// var nam = 1;
+// while(num <=10){
+//     console.log(num);
+//     num++
+// }
+
+// var num = 10;
+// while(num >= 1){
+//     console.log(num)
+//     num--;
+    
+// }
+
+// var i = 0;
+// for (i=0; i<5; i++){};
+// console.log(i);
+// var marks = [ 13, 15, 14, 20, 18];
+// for(var i=0; i<marks.length; i++){
+//   if(marks[i] >=15){
+//     continue;
+//   }
+//   console.log(marks[i]);
+// };
+
+// var i = 5;
+// for (i < 5; i++) {
+//  console.log(i);
+// }
+var i = 5;
+for (; i < 5; i++) {
+ console.log(i);
+}
