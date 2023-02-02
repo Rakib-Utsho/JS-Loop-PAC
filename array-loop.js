3436cf11ab5fb1a285a150d776c9d174f90746ae
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-03 01:09:04 +0600

    first commit

---
+++ array-loop.js
@@ -0,0 +1,12 @@
+// var i;
+// for(i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// display every elements of array using loop
+var numbers = [45,87,89,56,35,51,25,188,41,25,98];
+
+for(var i = 0;i<numbers.length;i++){
+    var number = numbers[i];
+    console.log(number);
+}
